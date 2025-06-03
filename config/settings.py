a6cab93939a1ae3833b5f0f7f501e5057b17674b
Yana Sozonova <ynakochneva@mail.ru>
2025-06-03 15:11:26 +0500

    создание модели пользователь

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(unique=True, verbose_name="Ник")
+    email = models.EmailField(verbose_name="Email", null=True, blank=True,)
+    phone = PhoneNumberField(verbose_name="Телефон", null=True, blank=True, help_text="Введите номер телефона")
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
+
