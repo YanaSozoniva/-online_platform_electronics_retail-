0cffebaafc23fa1482dfdabb8e15a835482e5b92
Yana Sozonova <ynakochneva@mail.ru>
2025-06-07 23:35:31 +0500

    настройка админ панели

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+from suppliers.models import Supplier, Order, Product
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    """Класс для настройки отображения модели поставщик"""
+
+    list_display = (
+        "id",
+        "name",
+        "email",
+        "country",
+        "address",
+        "city",
+        "type",
+    )
+    list_filter = ("city", "name")
+    search_fields = (
+        "city", "name",
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Класс для настройки отображения модели продукт"""
+
+    list_display = (
+        "id",
+        "name",
+        "model_product",
+        "release_date",
+    )
+    list_filter = ("name", )
+    search_fields = (
+        "name", "model_product",
+    )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """Класс для настройки отображения модели заказ"""
+
+    list_display = (
+        "id",
+        "customer",
+        "product",
+        "supplier",
+        "debt_to_supplier",
+        "created_at",
+        "level",
+    )
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-
+from suppliers.views import UserViewSet
 from users.apps import UserConfig
 
 app_name = UserConfig.name
