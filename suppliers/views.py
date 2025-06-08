5d638519d47eab957b9b6259fc072004255539a5
Yana Sozonova <ynakochneva@mail.ru>
2025-06-08 21:25:51 +0500

    добавлены ViewSet для всех моделей

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,2 +1,18 @@
+from rest_framework.viewsets import ModelViewSet
+from suppliers.models import Order, Product, Supplier
+from suppliers.serializers import ProductSerializer, OrderSerializer, SupplierSerializer
 
 
+class ProductViewSet(ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+class OrderViewSet(ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+
+class SupplierViewSet(ModelViewSet):
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objects.all()
